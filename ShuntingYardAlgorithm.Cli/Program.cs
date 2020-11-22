0e4751969e210709cc3e3b947a326e8526b3dd45
cainy-a <cainatkinson98@gmail.com>
2020-11-22 21:51:22 +0000

    Add CLI interface

---
+++ ShuntingYardAlgorithm.Cli/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace ShuntingYardAlgorithm.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an infix-notation equation: ");
+            var infix = Console.ReadLine();
+            var yard = new ShuntingYard(infix);
+            if (!yard.ShuntValid)
+            {
+                Console.WriteLine("Input equation is empty");
+                return;
+            }
+            yard.Shunt();
+            var sb = new StringBuilder();
+            sb.AppendJoin(' ', yard.Output.Select(s => s.ToString()).ToArray());
+            var rpn = sb.ToString();
+            Console.WriteLine("Your Reverse Polish Notation / Postfix Notation equation is:\n" + rpn);
+        }
+    }
+}
